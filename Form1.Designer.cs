b52d9de64eb132407309b5b7ba24afae63be6d42
kpwea <kpwea@DESKTOP-Q9V7JVJ>
2023-10-03 13:01:34 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace IT481_Weaver_Unit3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 437);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Connect";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(279, 437);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(289, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Connect to DB";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 502);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(203, 36);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Count Customer";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(31, 568);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(203, 34);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Get Customers";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(593, 388);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(181, 34);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Count Order";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(593, 442);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(164, 34);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Get Orders";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(593, 507);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(187, 34);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Count Employees";
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(593, 573);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(164, 34);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Get Employees";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(279, 501);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(289, 37);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Get Customer Count";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(279, 568);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(289, 34);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Get Customers";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(788, 388);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(289, 34);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Get Order Count";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(788, 442);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(289, 34);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Get Orders";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(788, 507);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(289, 34);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "Get Employee Count";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(788, 573);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(289, 34);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "Get Employees";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(279, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(289, 20);
+            this.textBox1.TabIndex = 14;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(279, 88);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(289, 20);
+            this.textBox2.TabIndex = 15;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(279, 140);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(289, 20);
+            this.textBox3.TabIndex = 16;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(279, 200);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(289, 20);
+            this.textBox4.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(0, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(100, 23);
+            this.label8.TabIndex = 0;
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(31, 37);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(118, 34);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "User";
+            // 
+            // label10
+            // 
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(31, 88);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(118, 34);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Password";
+            // 
+            // label11
+            // 
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(31, 140);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(118, 34);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "Server";
+            // 
+            // label12
+            // 
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(31, 194);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(118, 34);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "Database";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1107, 647);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System.Windows.Forms;
+using System;
+using System.Web.Mvc;
+using System.Collections;
+using IT481_Weaver_Unit03;
+using System.Linq;
+
+namespace IT481_Weaver_Unit3
+{
+    public partial class Form1 : Form
+    {
+        DB database;
+        private string user;
+        private string password;
+        private string server;
+        private string dbase;
+        public Form1()
+        {
+
+            InitializeComponent();
+            button1.Click += new EventHandler(button1_Click);
+            button2.Click += new EventHandler(button2_Click);
+            button3.Click += new EventHandler(button3_Click);
+            button4.Click += new EventHandler(button4_Click);
+            button5.Click += new EventHandler(button5_Click);
+            button6.Click += new EventHandler(button6_Click);
+            button7.Click += new EventHandler(button7_Click);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool isValid = true;
+            user = textBox1.Text;
+            password = textBox2.Text;
+            server = textBox3.Text;
+            dbase = textBox4.Text;
+
+            if (user.Length == 0 || password.Length == 0 ||
+                server.Length == 0 || dbase.Length == 0)
+            {
+                isValid = false;
+                MessageBox.Show("You must enter user name, password, server, and database values");
+            }
+            ///New Password Requirements
+            else if(password.Length < 12)
+            {
+                isValid = false;
+                MessageBox.Show("Password length must be 12 characters or more");
+            }
+            else
+            {
+                if (password.All(char.IsLetterOrDigit) && password.Any(ch => !char.IsLetterOrDigit(ch)))
+                {
+                    isValid = false;
+                    MessageBox.Show("You must enter alphanumeric and special characters for the password.");
+                }
+            }
+            //End of New Password Requirements
+            if (isValid)
+            {
+                MessageBox.Show("Connection Information Sent");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string count = database.getCustomerCount();
+            MessageBox.Show(count, "Customer Count");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string count = database.getCustomerNames();
+            MessageBox.Show(count, "Customer Names");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string count = database.getOrderCount();
+            MessageBox.Show(count, "Customer Count");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string count = database.getOrder();
+            MessageBox.Show(count, "OrderID");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string count = database.getEmployeeCount();
+            MessageBox.Show(count, "Employee Count");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string count = database.getEmployeeLastName();
+            MessageBox.Show(count, "Employee Last Name");
+        }
+
+    }
+}
